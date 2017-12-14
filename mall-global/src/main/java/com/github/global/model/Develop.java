a6bb9b71fd31e9ab343c966af36f24ab63ae33ff
liuanxin <liujie@wolianw.com>
2017-12-14 18:23:27 +0800

    fix

--- mall-global/src/main/java/com/github/global/model/Develop.java
+++ mall-global/src/main/java/com/github/global/model/Develop.java
@@ -2,9 +2,12 @@ package com.github.global.model;
 
 public class Develop {
 
+    /** 文档相关的说明 */
     public static final String TITLE = "xxx 项目文档";
     public static final String CONTACT = "联系";
     public static final String TEAM = "团队";
+
+    // 下面是具体的开发人员
 
     public static final String COMMON = "Python";
     public static final String USER = "Canal";
