1569ac1440ea80db9eb1ac95e61a5ecd6bf73ecf
liujie <liujie@wolianw.com>
2018-01-23 18:24:05 +0800

    fix

--- mall-global/src/main/java/com/github/global/enums/Gender.java
+++ mall-global/src/main/java/com/github/global/enums/Gender.java
@@ -2,12 +2,16 @@ package com.github.global.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.github.common.util.A;
 import com.github.common.util.U;
+import com.google.common.primitives.Ints;
+
+import java.util.Map;
 
 /** 性别 */
 public enum Gender {
 
-    Male(0, "男"), Female(1, "女");
+    Male(0, "男"), Female(1, "女"), Nil(3, "未知"), Other(4, "其他");
 
     int code;
     String value;
@@ -29,5 +33,14 @@ public enum Gender {
     @JsonCreator
     public static Gender deserializer(Object obj) {
         return U.toEnum(Gender.class, obj);
+    }
+
+    /** 返回给前台的下拉数据. 当需要自定义时使用此种方式 */
+    public static Map<String, Object> select() {
+        return A.linkedMaps(
+                Male.code, Male.value,
+                Female.code, Female.value,
+                Ints.join(",", Nil.code, Other.code), Other.value
+        );
     }
 }
