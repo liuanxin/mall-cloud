474407dfc7cf2acc931466d8cb3564986ab474e1
liujie <liujie@wolianw.com>
2018-03-13 13:19:30 +0800

    fix

--- web-backend/src/main/java/com/github/WebBackendApplication.java
+++ web-backend/src/main/java/com/github/WebBackendApplication.java
@@ -1,10 +1,13 @@
 package com.github;
 
+import com.github.common.util.A;
+import com.github.common.util.LogUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ApplicationContext;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -17,6 +20,12 @@ public class WebBackendApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder().sources(WebBackendApplication.class).run(args);
+        ApplicationContext ctx = new SpringApplicationBuilder().sources(WebBackendApplication.class).run(args);
+        if (LogUtil.ROOT_LOG.isDebugEnabled()) {
+            String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+            if (A.isNotEmpty(activeProfiles)) {
+                LogUtil.ROOT_LOG.debug("current profile : ({})", A.toStr(activeProfiles));
+            }
+        }
     }
 }
--- web-manager/src/main/java/com/github/WebManagerApplication.java
+++ web-manager/src/main/java/com/github/WebManagerApplication.java
@@ -1,10 +1,13 @@
 package com.github;
 
+import com.github.common.util.A;
+import com.github.common.util.LogUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
 
 @EnableDiscoveryClient
 @EnableFeignClients
@@ -17,6 +20,12 @@ public class WebManagerApplication extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder().sources(WebManagerApplication.class).run(args);
+        ApplicationContext ctx = new SpringApplicationBuilder().sources(WebManagerApplication.class).run(args);
+        if (LogUtil.ROOT_LOG.isDebugEnabled()) {
+            String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+            if (A.isNotEmpty(activeProfiles)) {
+                LogUtil.ROOT_LOG.debug("current profile : ({})", A.toStr(activeProfiles));
+            }
+        }
     }
 }
