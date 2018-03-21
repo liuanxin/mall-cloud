ff791b44edaecc9e5507b3c8a58cb49221a91733
liujie <liujie@wolianw.com>
2018-03-21 17:36:04 +0800

    fix

--- mall-common/src/main/java/com/github/common/json/JsonCode.java
+++ mall-common/src/main/java/com/github/common/json/JsonCode.java
@@ -4,16 +4,16 @@ package com.github.common.json;
 public enum JsonCode {
 
     /** 显示 msg 给用户看 */
-    FAIL(500, "失败"),
+    FAIL(500, "失败. 显示 msg 给用户看"),
 
     /** 将 data 解析后渲染页面(依业务而定, 也可能显示 msg 给用户看, 如 收货地址添加成功 这种) */
-    SUCCESS(200, "成功"),
+    SUCCESS(200, "成功. 操作数据或显示 msg 给用户看, 依具体的业务而定"),
 
     /** 导向登录页面引导用户登录 */
-    NOT_LOGIN(401, "未登录"),
+    NOT_LOGIN(401, "未登录, 导到登录页"),
 
     /** 提示用户无权限. 处理跟 失败 是一样的 */
-    NOT_PERMISSION(403, "无权限");
+    NOT_PERMISSION(403, "无权限, 提示用户无权限");
 
     int flag;
     String msg;
