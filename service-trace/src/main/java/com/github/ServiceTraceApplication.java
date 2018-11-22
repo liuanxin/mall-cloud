a07189281bb39bbbab66756841bc21f0e3686642
liujie <liu_anxin@163.com>
2018-11-22 11:29:39 +0800

    fix

--- service-trace/src/main/java/com/github/ServiceTraceApplication.java
+++ service-trace/src/main/java/com/github/ServiceTraceApplication.java
@@ -2,9 +2,8 @@ package com.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
 
-@EnableZipkinServer
+//@EnableZipkinServer
 @SpringBootApplication
 public class ServiceTraceApplication {
 
