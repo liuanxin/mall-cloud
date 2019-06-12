0f7e62f386996b7f3759d42688f654c6a86a11b1
liuanxin <liu_anxin@163.com>
2019-06-12 16:38:40 +0800

    Update CacheService.java

--- mall-global/src/main/java/com/github/global/service/CacheService.java
+++ mall-global/src/main/java/com/github/global/service/CacheService.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.util.ReflectionUtils;
+import org.springframework.data.redis.core.types.Expiration;
 import redis.clients.jedis.Jedis;
+import redis.clients.util.SafeEncoder;
 
-import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -21,9 +21,6 @@ import java.util.concurrent.TimeUnit;
 @ConditionalOnClass({ Jedis.class, RedisTemplate.class })
 @ConditionalOnBean({ RedisTemplate.class, StringRedisTemplate.class })
 public class CacheService {
-
-    @Autowired
-    private RedisConnectionFactory connectionFactory;
 
     /** @see org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration */
     @Autowired
@@ -37,8 +34,8 @@ public class CacheService {
         stringRedisTemplate.opsForValue().set(key, value);
     }
     /** 往 redis 放值, 并设定超时时间 */
-    public void set(String key, String value, long timeOut, TimeUnit timeUnit) {
-        stringRedisTemplate.opsForValue().set(key, value, timeOut, timeUnit);
+    public void set(String key, String value, long time, TimeUnit unit) {
+        stringRedisTemplate.opsForValue().set(key, value, time, unit);
     }
     /** 往 redis 放值, 并设定超时时间 */
     public void set(String key, String value, Date expireTime) {
@@ -49,39 +46,69 @@ public class CacheService {
     }
     /**
      * <pre>
-     * 向 redis 中原子存放一个值, 成功则返回 true, 否则返回 false, 想要操作分布式锁, 可以像下面这样操作
+     * 用 redis 获取分布式锁, 获取成功则返回 true. 想要操作分布式锁, 可以像下面这样操作
      *
-     * String key = xxx;
-     * String value = uuid();
-     * long seconds = yyy;
-     * boolean flag = cacheService.setIfNotExists(key, value, seconds);
-     * if (flag) {
+     * String key = "xxx", value = uuid(); // value 用 uuid 确保每个线程都不一样
+     * boolean lock = tryLock(key, value, 10L, TimeUnit.SECONDS);
+     * // 如果获取失败这里会是 false
+     * if (lock) {
      *   try {
      *     // 获取到锁之后的业务处理
      *   } finally {
-     *     // 释放锁的时候先去缓存中取, 如果值跟之前存进去的一样才进行删除操作
-     *     // 避免当前线程执行太长, 超时后其他线程又设置了值在处理
-     *     // 如果当前线程 不获取并比较就直接删除, 会将其他线程获取到的锁信息也给删掉
-     *     String val = cacheService.get(key);
-     *     if (value.equals(val)) {
-     *       cacheService.delete(key);
-     *     }
+     *     // 解锁时 key 和 value 都需要
+     *     unlock(key, value);
      *   }
      * }
      * </pre>
      *
      * @param key 键
      * @param value 值
-     * @param seconds 超时时间, 单位: 秒
+     * @param time 超时时间
+     * @param unit 时间单位
      * @return 返回 true 则表示获取到了锁
      */
-    public boolean setIfNotExists(String key, String value, long seconds) {
-        Field jedisField = ReflectionUtils.findField(JedisConnection.class, "jedis");
-        ReflectionUtils.makeAccessible(jedisField);
-        Jedis jedis = (Jedis) ReflectionUtils.getField(jedisField, connectionFactory.getConnection());
+    public boolean tryLock(String key, String value, long time, TimeUnit unit) {
+         Boolean flag = stringRedisTemplate.execute((RedisCallback<Boolean>) connection -> {
+             byte[] byteKey = SafeEncoder.encode(key);
+             byte[] byteValue = SafeEncoder.encode(value);
+             Expiration expiration = Expiration.from(time, unit);
+             RedisStringCommands.SetOption option = RedisStringCommands.SetOption.ifAbsent();
+             
+             return connection.set(byteKey, byteValue, expiration, RedisStringCommands.SetOption.ifAbsent());
+         });
+         return U.isNotBlank(flag) && flag;
+    }
+    /**
+     * <pre>
+     * 用 redis 解分布式锁. 想要操作分布式锁, 可以像下面这样操作
+     *
+     * String key = "xxx", value = uuid(); // value 用 uuid 确保每个线程都不一样
+     * boolean lock = tryLock(key, value, 10L, TimeUnit.SECONDS);
+     * // 如果获取失败这里会是 false
+     * if (lock) {
+     *   try {
+     *     // 获取到锁之后的业务处理
+     *   } finally {
+     *     // 解锁时 key 和 value 都需要
+     *     unlock(key, value);
+     *   }
+     * }
+     * </pre>
+     *
+     * @param key 键
+     * @param value 值
+     */
+    public void unlock(String key, String value) {
+        // 释放锁的时候先去缓存中取, 如果值跟之前存进去的一样才进行删除操作, 避免当前线程执行太长, 超时后其他线程又设置了值在处理
+        String val = get(key);
+        if (value.equals(val)) {
+            delete(key);
+        }
+    }
 
-        String result = jedis.set(key, value, "NX", "EX", seconds);
-        return U.isNotBlank(result) && "OK".equalsIgnoreCase(result);
+    /** 设置超时 */
+    public void expire(String key, int time, TimeUnit unit) {
+        redisTemplate.expire(key, time, unit);
     }
 
     /** 从 redis 中取值 */
@@ -101,15 +128,21 @@ public class CacheService {
     public Object pop(Object key) {
         return redisTemplate.opsForList().rightPop(key);
     }
+    /** 向队列读值(从右边出栈, 阻塞指定的时间) */
+    public Object pop(Object key, int time, TimeUnit unit) {
+        return redisTemplate.opsForList().rightPop(key, time, unit);
+    }
 
 
     /** 获取指定 set 的长度: scard key */
     public long setSize(String key) {
-        return stringRedisTemplate.opsForSet().size(key);
+        Long size = stringRedisTemplate.opsForSet().size(key);
+        return U.greater0(size) ? size : 0L;
     }
     /** 将指定的 set 存进 redis 的 set 并返回成功条数: sadd key v1 v2 v3 ... */
     public long setAdd(String key, String[] set) {
-        return stringRedisTemplate.opsForSet().add(key, set);
+        Long add = stringRedisTemplate.opsForSet().add(key, set);
+        return U.greater0(add) ? add : 0L;
     }
     /** 从指定的 set 中随机取一个值: spop key */
     public Object setPop(String key) {
