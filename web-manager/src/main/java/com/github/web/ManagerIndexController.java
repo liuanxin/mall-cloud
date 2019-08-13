bc7dbcd1ace7266c28cfc896c41b57e79786729f
liujie <liu_anxin@163.com>
2019-08-13 18:39:05 +0800

    fix

--- web-manager/src/main/java/com/github/web/ManagerIndexController.java
+++ web-manager/src/main/java/com/github/web/ManagerIndexController.java
@@ -4,7 +4,9 @@ import com.github.common.RenderViewResolver;
 import com.github.common.json.JsonResult;
 import com.github.common.util.SecurityCodeUtil;
 import com.github.common.util.U;
+import com.github.global.constant.Develop;
 import com.github.liuanxin.api.annotation.ApiIgnore;
+import com.github.liuanxin.api.annotation.ApiMethod;
 import com.github.liuanxin.api.annotation.ApiParam;
 import com.github.liuanxin.api.annotation.ApiTokens;
 import com.github.util.ManagerDataCollectUtil;
@@ -33,6 +35,7 @@ public class ManagerIndexController {
 
     @ApiIgnore(false)
     @ApiTokens(false)
+    @ApiMethod(value = "枚举信息", develop = Develop.COMMON)
     @GetMapping("/enum")
     @ResponseBody
     public JsonResult enumList(@ApiParam("枚举类型. 不传则返回所有列表, 多个以逗号分隔") String type) {
